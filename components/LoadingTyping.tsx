408110d6f62ca6e84b9e899e97d00c0cd3989452
Supanut1911 <ynotmela@gmail.com>
2023-04-24 04:01:06 +0700

    :lipstick: - add loading typing animation

---
+++ components/LoadingTyping.tsx
@@ -0,0 +1,14 @@
+const LoadingTyping = () => {
+  return (
+    <>
+      <div className="flex items-center space-x-2">
+        <p className="font-bold text-xl">Thinking</p>
+        <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse"></div>
+        <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-75"></div>
+        <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-150"></div>
+      </div>
+    </>
+  );
+};
+
+export default LoadingTyping;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import LoadingTyping from "@/components/LoadingTyping";
 import axios from "axios";
 import { log } from "console";
 import { useEffect, useState } from "react";
@@ -56,51 +57,60 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto mx-w-[800px]">
-      <div className="flex flex-col h-screen bg-gray-800">
-        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
-          Hey yo GPT!
-        </h1>
-        <div className="flex-grow p-8">
-          <div className="flex flex-col space-y-4">
-            {chatLogs.map((message, idx) => (
-              <div
-                key={idx}
-                className={`flex ${
-                  message.type === "user" ? "justify-end" : "justify-start"
-                } `}
-              >
+    <div className="bg-gradient-to-r from-red-500 to-purple-500">
+      <div className="container mx-auto mx-w-[800px]">
+        <div className="flex flex-col h-screen overflow-auto bg-gray-800">
+          <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
+            Hey yo GPT!
+          </h1>
+          <div className="flex-grow p-8">
+            <div className="flex flex-col space-y-4">
+              {chatLogs.map((message, idx) => (
                 <div
-                  className={`${
-                    message.type === "user" ? "bg-purple-500" : "bg-gray-500"
-                  } rounded-lg p-4 text-white max-w-sm`}
+                  key={idx}
+                  className={`flex ${
+                    message.type === "user" ? "justify-end" : "justify-start"
+                  } `}
                 >
-                  {message.message}
+                  <div
+                    className={`${
+                      message.type === "user" ? "bg-purple-500" : "bg-gray-500"
+                    } rounded-lg p-4 text-white max-w-sm`}
+                  >
+                    {message.message}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+              {isLoading && (
+                <div key={chatLogs.length} className="flex justify-start">
+                  <div className="bg-gray-700 rounded-lg p-4 text-white max-w-sm">
+                    <LoadingTyping />
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
 
-        <form onSubmit={handleSubmit} className="flex-none p-8">
-          <div className="flex rounded-lg border border-gray-600 bg-gray-700">
-            <input
-              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
-              type="text"
-              placeholder="type something..."
-              value={inputValue}
-              onChange={(e) => {
-                setInputValue(e.target.value);
-              }}
-            />
-            <button
-              type="submit"
-              className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
-            >
-              Go
-            </button>
-          </div>
-        </form>
+          <form onSubmit={handleSubmit} className="flex-none p-8">
+            <div className="flex rounded-lg border border-gray-600 bg-gray-700">
+              <input
+                className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
+                type="text"
+                placeholder="type something..."
+                value={inputValue}
+                onChange={(e) => {
+                  setInputValue(e.target.value);
+                }}
+              />
+              <button
+                type="submit"
+                className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
+              >
+                Go
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
