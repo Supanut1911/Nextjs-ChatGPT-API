fb81b4fb0c7488fe54f1605da320cfe39ffaf9d3
Supanut1911 <ynotmela@gmail.com>
2023-04-24 03:39:33 +0700

    :lipstick: - add ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,23 +56,52 @@ export default function Home() {
   };
 
   return (
-    <>
-      <p>Hi GPT</p>
-      {chatLogs.map((message, idx) => (
-        <div key={idx}>{message.message}</div>
-      ))}
+    <div className="container mx-auto mx-w-[800px]">
+      <div className="flex flex-col h-screen bg-gray-800">
+        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
+          Hey yo GPT!
+        </h1>
+        <div className="flex-grow p-8">
+          <div className="flex flex-col space-y-4">
+            {chatLogs.map((message, idx) => (
+              <div
+                key={idx}
+                className={`flex ${
+                  message.type === "user" ? "justify-end" : "justify-start"
+                } `}
+              >
+                <div
+                  className={`${
+                    message.type === "user" ? "bg-purple-500" : "bg-gray-500"
+                  } rounded-lg p-4 text-white max-w-sm`}
+                >
+                  {message.message}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="type something..."
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        />
-        <button type="submit">Go</button>
-      </form>
-    </>
+        <form onSubmit={handleSubmit} className="flex-none p-8">
+          <div className="flex rounded-lg border border-gray-600 bg-gray-700">
+            <input
+              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
+              type="text"
+              placeholder="type something..."
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
+            >
+              Go
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
